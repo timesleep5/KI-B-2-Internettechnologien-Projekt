e342a5b98748ca4f725fbcb28cf3193fda4c9f12
Korbi PC <korbi03@icloud.com>
2024-06-10 23:53:31 +0200

    frontend: implement chat

--- frontend/src/app/chat.service.ts
+++ frontend/src/app/chat.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {catchError, Observable, of} from "rxjs";
 import {ChatSession} from "./models/chat-session";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {BotMessage, UserMessage} from "./models/message";
 
 @Injectable({
   providedIn: 'root'
@@ -18,9 +19,9 @@ export class ChatService {
   ) {
   }
 
-  createChatFromName(name: string): Observable<number> {
-    const url = `${this.BASE_URL}/new/name/${name}`;
-    return this.http.get<number>(url)
+  createChatSessionFromName(name: string): Observable<number> {
+    const url = `${this.BASE_URL}/new?name=${name}`;
+    return this.http.post<number>(url, this.httpOptions)
       .pipe(
         catchError(this.handleError<number>(`createChatFromName name=${name}`))
       )
@@ -32,6 +33,16 @@ export class ChatService {
       .pipe(
         catchError(this.handleError<ChatSession>(`getChat id=${id}`))
       );
+  }
+
+  sendMessage(chatId: number, message: UserMessage): Observable<BotMessage> {
+    const url = `${this.BASE_URL}/id/${chatId}/message`;
+    const serializedMessage = JSON.stringify(message);
+    console.log(serializedMessage);
+    return this.http.post<BotMessage>(url, message, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<BotMessage>(`sendMessage to chat id=${chatId}`))
+      )
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -2,19 +2,34 @@ import {Component, OnInit} from '@angular/core';
 import {ChatService} from "../chat.service";
 import {ActivatedRoute} from "@angular/router";
 import {ChatSession} from "../models/chat-session";
+import {BotMessage, Message, UserMessage} from "../models/message";
+import {DatePipe, NgClass, NgForOf} from "@angular/common";
+import {User} from "../models/user";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgForOf,
+    DatePipe,
+    FormsModule
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent implements OnInit {
+  private chatId: number;
+  private chatSession?: ChatSession;
+  protected user?: User;
+  protected newMessageContent: string = '';
+
   constructor(
     private route: ActivatedRoute,
     private chatService: ChatService,
   ) {
+    this.chatId = 0;
   }
 
   ngOnInit(): void {
@@ -22,10 +37,47 @@ export class ChatComponent implements OnInit {
   }
 
   private getChat(): void {
-    const chat_id: number = Number(this.route.snapshot.paramMap.get('id'));
-    this.chatService.getChat(chat_id)
+    this.chatId = Number(this.route.snapshot.paramMap.get('id'));
+    this.chatService.getChat(this.chatId)
       .subscribe((chat: ChatSession) => {
-        console.log(chat.user.name)
-      })
+        this.chatSession = chat
+        this.user = chat.user;
+      });
+  }
+
+  sendMessage(): void {
+    if (this.newMessageContent.trim()) {
+      const userMessage: UserMessage = this.createUserMessage()
+      this.chatSession?.messages.push(userMessage);
+      this.newMessageContent = '';
+
+      this.chatService.sendMessage(this.chatId, userMessage)
+        .subscribe((botResponse: BotMessage) => {
+            if (botResponse) {
+              this.chatSession?.messages.push(botResponse);
+            }
+          }
+        );
+    }
+  }
+
+  private createUserMessage(): UserMessage {
+    return {
+      time_sent: new Date(),
+      content: this.newMessageContent.trim(),
+      user: this.chatSession?.user
+    } as UserMessage;
+  }
+
+  getMessages(): Message[] {
+    if (this.chatSession) {
+      return this.chatSession.messages;
+    } else {
+      return []
+    }
+  }
+
+  isUserMessage(message: Message): boolean {
+    return 'user' in message;
   }
 }
