144a181b55e181c2429468b9c307e3cb3333aed3
Korbi PC <korbi03@icloud.com>
2024-06-10 23:44:11 +0200

    frontend: add user creation and forwarding to chat to welcome component

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {ChatService} from "../chat.service";
+import {NgIf} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
 
+  currentChatId?: number;
+
+  constructor(
+    private chatService: ChatService,
+    private router: Router
+  ) {
+  }
+
+  createChatSessionFromName(name: string): void {
+    this.chatService.createChatSessionFromName(name)
+      .subscribe((chatId: number) => {
+          this.currentChatId = chatId;
+          this.router.navigateByUrl(`/chat/${this.currentChatId}`);
+        }
+      )
+  }
 }
