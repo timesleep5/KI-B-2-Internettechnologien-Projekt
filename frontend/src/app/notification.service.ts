f6f20265813eda11257a4711a0d30944487e0213
Korbi PC <korbi03@icloud.com>
2024-06-11 15:24:00 +0200

    frontend: add notification service

---
+++ frontend/src/app/notification.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {ToastrService} from "ngx-toastr";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(
+    private toastr: ToastrService,
+  ) {
+  }
+
+  showSuccess(message: string, title: string): void {
+    this.toastr.success(message, title);
+  }
+
+  showError(message: string, title: string): void {
+    this.toastr.error(message, title);
+  }
+}
