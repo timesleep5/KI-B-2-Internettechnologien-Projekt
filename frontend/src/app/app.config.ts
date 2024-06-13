fba447e42f8b2063dc4ec4c015c83a0e2689c8f7
Korbi PC <korbi03@icloud.com>
2024-06-13 15:11:21 +0200

    frontend: declare base url in app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,6 +7,8 @@ import {provideHttpClient} from "@angular/common/http";
 import {provideToastr} from "ngx-toastr";
 import {provideAnimations} from "@angular/platform-browser/animations";
 
+export const BASE_URL = "http://127.0.0.1:8080";
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
--- frontend/src/app/chat.service.ts
+++ frontend/src/app/chat.service.ts
@@ -3,24 +3,26 @@ import {catchError, Observable, of} from "rxjs";
 import {ChatSession} from "./models/chat-session";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {BotMessage, UserMessage} from "./models/message";
+import {BASE_URL} from "./app.config";
+import {NotificationService} from "./notification.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  readonly BASE_URL: string = "http://127.0.0.1:8080/chats";
-
-  httpOptions = {
+  readonly SERVICE_URL: string = `${BASE_URL}/chats`;
+  private httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   }
 
   constructor(
     private http: HttpClient,
+    private notificationService: NotificationService,
   ) {
   }
 
   createChatSessionFromName(name: string): Observable<number> {
-    const url = `${this.BASE_URL}/new?name=${name}`;
+    const url = `${this.SERVICE_URL}/new?name=${name}`;
     return this.http.post<number>(url, this.httpOptions)
       .pipe(
         catchError(this.handleError<number>(`createChatFromName name=${name}`))
@@ -28,7 +30,7 @@ export class ChatService {
   }
 
   getChat(id: number): Observable<ChatSession> {
-    const url = `${this.BASE_URL}/id/${id}`;
+    const url = `${this.SERVICE_URL}/id/${id}`;
     return this.http.get<ChatSession>(url)
       .pipe(
         catchError(this.handleError<ChatSession>(`getChat id=${id}`))
@@ -36,9 +38,7 @@ export class ChatService {
   }
 
   sendMessage(chatId: number, message: UserMessage): Observable<BotMessage> {
-    const url = `${this.BASE_URL}/id/${chatId}/message`;
-    const serializedMessage = JSON.stringify(message);
-    console.log(serializedMessage);
+    const url = `${this.SERVICE_URL}/id/${chatId}/message`;
     return this.http.post<BotMessage>(url, message, this.httpOptions)
       .pipe(
         catchError(this.handleError<BotMessage>(`sendMessage to chat id=${chatId}`))
@@ -48,7 +48,7 @@ export class ChatService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
-
+      this.notificationService.showError(`${operation} failed`, 'ChatService Error')
       return of(result as T);
     }
   }
