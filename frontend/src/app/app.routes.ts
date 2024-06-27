c38e25e5bb6caead279a5f71aba298a72bc70c88
Korbi PC <korbi03@icloud.com>
2024-06-27 19:31:02 +0200

    frontend: routes: adjust routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {ChatComponent} from "./chat/chat.component";
 import {ImprintComponent} from "./imprint/imprint.component";
+import {ChatNotFoundComponent} from "./chat-not-found/chat-not-found.component";
+import {DocumentationComponent} from "./documentation/documentation.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'chat/:id', component: ChatComponent},
+  {path: 'chat', component: ChatComponent},
+  {path: 'chat-not-found', component: ChatNotFoundComponent},
   {path: 'imprint', component: ImprintComponent},
+  {path: 'doc', component: DocumentationComponent}
 ];
