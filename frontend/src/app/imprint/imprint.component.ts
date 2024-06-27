24138642348f9768c28f42729ee4b439e46df75e
Korbi PC <korbi03@icloud.com>
2024-06-27 19:28:33 +0200

    frontend: imprint: add content, add imprint.json

--- frontend/src/app/imprint/imprint.component.ts
+++ frontend/src/app/imprint/imprint.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ImprintService} from "../imprint.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.css'
 })
-export class ImprintComponent {
+export class ImprintComponent implements OnInit {
+  imprintData: any;
 
+  constructor(
+    private imprintService: ImprintService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getImprintDataFromBackend();
+  }
+
+  private getImprintDataFromBackend(): void {
+    this.imprintService.getImprintData().subscribe(data => {
+      this.imprintData = data;
+    })
+  }
+
+  getImprintData() {
+    this.getImprintDataFromBackend();
+    return this.imprintData;
+  }
 }
